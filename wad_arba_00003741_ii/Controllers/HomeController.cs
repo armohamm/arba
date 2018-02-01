5b410940dee55e06e5d4cd4256151e9b03850480
Abdurakhmon <milano95a@gmail.com>
2018-02-02 00:21:38 +0500

    v3 add product view, back-end validation

--- wad_arba_00003741_ii/Controllers/HomeController.cs
+++ wad_arba_00003741_ii/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wad_arba_00003741_ii.Models;
 
 namespace wad_arba_00003741_ii.Controllers
 {
@@ -13,5 +14,21 @@ namespace wad_arba_00003741_ii.Controllers
         {
             return View();
         }
+        
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View(new ProductViewModel());
+        }   
+        
+        [HttpPost]
+        public ActionResult AddProduct(ProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(new ProductViewModel());
+        } 
     }
 }
---
+++ wad_arba_00003741_ii/Models/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace wad_arba_00003741_ii.Models
+{
+    public class ProductViewModel
+    {
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 999999)]
+        public float Price { get; set; }
+
+        [Required]    
+        public string Category { get; set; }
+
+        [DisplayName("Available")]
+        public bool IsActive { get; set; }
+    }
+}
