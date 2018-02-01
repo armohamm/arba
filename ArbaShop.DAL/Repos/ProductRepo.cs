90323629c692b3e58a61f048d07a87a5bbd77d92
Abdurakhmon <milano95a@gmail.com>
2018-02-02 01:25:16 +0500

    v5 created log, user tables, entity framework, product repo

---
+++ ArbaShop.DAL/Repos/ProductRepo.cs
@@ -0,0 +1,30 @@
+﻿using ArbaShop.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbaShop.DAL.Repos
+{
+    class ProductRepo
+    {
+        private ArbaShopDbEntities db;
+
+        public ProductRepo()
+        {
+            db = new ArbaShopDbEntities();
+        }
+
+        public IQueryable<Product> GetAll()
+        {
+            return db.Products;
+        }
+
+        public void Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();        
+        }
+    }
+}
